94553cf16e5a51287df49c11ad373ac3c7f2d0c6
MAlmeida98 <38048378+MAlmeida98@users.noreply.github.com>
2018-06-22 17:31:23 -0300

    Update P5.c

--- P5.c
+++ P5.c
@@ -1,41 +1,58 @@
 //Nome:Mateus Augusto Ferreira Almeida
 //Matricula:11811EEL023
-#include<stdio.h>
-int main(){
-	int o;
-	printf("1. Codificar\n2. Decodificar:\n");
-	scanf("%d",&o);getchar();
-	if(o==1){
-		char str[256];
-		int *pc,i=0;
-		
-		printf("O que quer codificar: ");
-		gets(str);
-		fflush(stdin);
-		
-		printf("A palavra codificada e: ");
-		for(pc=(int*)str; pc < ((int *) str) + sizeof(str); pc++){
-			if(*pc==0)
-				break;
-			printf("%d, ",*pc);
-		}
-		
+#include <stdio.h>
+
+void codificar()
+{
+	int i, j;
+	char str[256];
+	int *ps;
+	for(i=0; i<256; i++)
+	{
+		str[i]='\0';
 	}
-	if(o==2){
-		char *pd;
-		int vet[10];
-		int j,n=0;
-		printf("O que quer decodificar: ");
-		for(j=0;j<sizeof(char);j++){
-			scanf("%d, ",&vet[j]);
-			getchar();
-		}
-		for(n;n<sizeof(char);n++)
-		pd=((char*)&vet[n])-sizeof(vet)/sizeof(int);
-		for(;pd<(char*)&vet+sizeof(vet);pd++){
-			
-			printf("%c",*pd);
-		}
-		printf("%d",vet[1]);
+	printf("\n Digite algo em caracteres: ");
+	fgets(str, 256, stdin);
+	for(i=0; str[i]!='\0'; i++)
+	ps = (int*)str;
+	printf("\n O que voce digitou codificado eh: ");
+	for(j=0; j<i; j++)
+	{
+		if(*(ps+j) != 0)
+		printf("%i ", *(ps+j));
 	}
+}
+
+void decodificar()
+{
+	int vet[64], i, num;
+	char *pc;
+			pc = (char*)vet;
+			i=0; 
+			printf("\nDigite algo em numeros, aperte enter e digite 0 : ");
+			do{
+				scanf("%d", &num);
+				getchar();
+				vet[i] = num;
+				i++;
+			}while(num!=0);
+			pc = (char*)&vet;
+			printf("\n\n O que voce digitou decodificado eh: ");
+			for(i=0; i<sizeof(vet) && *(pc+i)!=0; i++)
+			{
+				printf("%c", *(pc+i));	
+			}
+}
+
+int main ()
+{
+	int opc;
+	printf("\n\n Bem vindo! \n\n 1-Codificar\n 2-Decodificar \n\n Sua opcao: ");
+	scanf("%d", &opc);
+	getchar();
+	if(opc==1)
+	codificar();
+	if(opc==2);
+	decodificar();
+return 0;
 }
