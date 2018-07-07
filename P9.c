381c77cf48612dcb49eaab2c06932efb74cdb9d4
MAlmeida98 <38048378+MAlmeida98@users.noreply.github.com>
2018-07-07 10:45:05 -0300

    Update P9.c

--- P9.c
+++ P9.c
@@ -1,61 +1,75 @@
 // Aluno: Mateus Augusto Ferreira Almeida
 // Matricula: 11811EEL023
 
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-
-struct Dimensoes{
-	float largura, profundidade, altura;
-};
-
-struct Produto{
-	char nome[64];
-	float preco;
-	struct Dimensoes dimensoes;
-};
-
-void cadastrar(struct Produto Quant[], int i)
-{
-	scanf("%s", &Quant[i].nome);
-	scanf("%f", &Quant[i].preco);
-	scanf("%f", &Quant[i].dimensoes.largura);
-	scanf("%f", &Quant[i].dimensoes.profundidade);
-	scanf("%f", &Quant[i].dimensoes.altura);
-	printf("Produto %d cadastrado com sucesso!\n", i+1);
-}
-
-void consultar(struct Produto Quant[], int i)
-{
-
-	if (strcmp (Quant[i].nome, "") != 0 && (i==0||i==1))
-		printf("%s, R$ %.2f, L: %.2fm x P: %.2fm x A: %.2fm\n", Quant[i].nome, Quant[i].preco, +
-			+ Quant[i].dimensoes.largura, Quant[i].dimensoes.profundidade, Quant[i].dimensoes.altura);
-	else
-		printf("Produto nao cadastrado!\n");
-		
-}
-
-int main ()
-{
-	struct Produto Quant[2];
-	strcpy(Quant[0].nome, "");
-	strcpy(Quant[1].nome, "");
-	int op, pro;
-	
-	printf("1 - Cadastro\n2 - Consulta\n");
-	
-	while(scanf("%d", &op)!=EOF)
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+typedef
+	struct Dimensoes
 	{
-		scanf("%d", &pro);
-		if(op==1)
-			cadastrar(Quant, pro-1);
-		else if(op==2)
-			consultar(Quant, pro-1);
-		else
-			printf("Entrada invalida!\n");
-
+		float largura;
+		float profundidade;
+		float altura;
 	}
-
+Dimensoes_m;
+typedef
+	struct Produto
+	{
+		char nome[64];
+		float preco;
+		Dimensoes_m dimensoes;
+	}
+Produto_m;
+void cadastro(int x, Produto_m p[2])
+{
+	printf("\nNome: ");
+	fgets(p[x].nome, 64, stdin); 
+	p[x].nome[strlen(p[x].nome)-1] = '\0';
+	printf("\nPreco: ");
+	scanf("%f", &p[x].preco);
+	getchar();
+	printf("\nLargura: ");
+	scanf("%f", &p[x].dimensoes.largura);
+	getchar();
+	printf("\nProfundidade: ");
+	scanf("%f", &p[x].dimensoes.profundidade);
+	getchar();
+	printf("\nAltura: ");
+	scanf("%f", &p[x].dimensoes.altura);
+	getchar();
+}
+int main()
+{
+	int op_1=0, op_2=0, i=0;
+	char aux[64];
+	Produto_m p[2];
+	for (i=0; i<2; i++)
+		p[i].nome[0] = '\0';
+	do
+	{
+		printf("\n1 - Cadastro\n2 - Consulta\n....");
+		scanf("%d", &op_1);
+		getchar();
+		switch (op_1)
+		{
+			case 1:
+				printf("\n....1 - Produto 1\n....2 - Produto 2\n........");
+				scanf("%d", &op_2);
+				getchar();
+				printf("\nCadastre as informacoes do produto %d: ", op_2);
+				cadastro(op_2-1, p);
+				printf("\nProduto %d cadastrado com sucesso", op_2);
+				break;
+			case 2:
+				printf("\nProduto a ser procurado: ");
+				scanf("%d", &op_2);
+				getchar();
+				if (strlen(p[op_2-1].nome) == 0)
+					printf("\nProduto nao cadastrado!\n");
+				else
+					printf("\n%s, R$ %.2f, L: %.2fm x P: %.2fm x A: %.2fm", p[op_2-1].nome, p[op_2-1].preco, p[op_2-1].dimensoes.largura, p[op_2-1].dimensoes.profundidade, p[op_2-1].dimensoes.altura);
+				break;
+		}
+	}while (op_1 == 1 || op_1 == 2);
 	return 0;
 }
