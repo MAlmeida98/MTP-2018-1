e2a8882b6e0b938f4965ecf88a8d0af4c72658b6
MAlmeida98 <38048378+MAlmeida98@users.noreply.github.com>
2018-07-10 18:32:08 -0300

    Update P9.c

--- P9.c
+++ P9.c
@@ -4,72 +4,112 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-typedef
-	struct Dimensoes
-	{
-		float largura;
-		float profundidade;
-		float altura;
-	}
-Dimensoes_m;
-typedef
-	struct Produto
-	{
-		char nome[64];
-		float preco;
-		Dimensoes_m dimensoes;
-	}
-Produto_m;
-void cadastro(int x, Produto_m p[2])
-{
-	printf("\nNome: ");
-	fgets(p[x].nome, 64, stdin); 
-	p[x].nome[strlen(p[x].nome)-1] = '\0';
-	printf("\nPreco: ");
-	scanf("%f", &p[x].preco);
-	getchar();
-	printf("\nLargura: ");
-	scanf("%f", &p[x].dimensoes.largura);
-	getchar();
-	printf("\nProfundidade: ");
-	scanf("%f", &p[x].dimensoes.profundidade);
-	getchar();
-	printf("\nAltura: ");
-	scanf("%f", &p[x].dimensoes.altura);
-	getchar();
+
+typedef struct Dimensao{
+    float largura;
+    float altura;
+    float profundidade;
 }
-int main()
-{
-	int op_1=0, op_2=0, i=0;
-	char aux[64];
-	Produto_m p[2];
-	for (i=0; i<2; i++)
-		p[i].nome[0] = '\0';
-	do
-	{
-		printf("\n1 - Cadastro\n2 - Consulta\n....");
-		scanf("%d", &op_1);
-		getchar();
-		switch (op_1)
-		{
-			case 1:
-				printf("\n....1 - Produto 1\n....2 - Produto 2\n........");
-				scanf("%d", &op_2);
-				getchar();
-				printf("\nCadastre as informacoes do produto %d: ", op_2);
-				cadastro(op_2-1, p);
-				printf("\nProduto %d cadastrado com sucesso", op_2);
-				break;
-			case 2:
-				printf("\nProduto a ser procurado: ");
-				scanf("%d", &op_2);
-				getchar();
-				if (strlen(p[op_2-1].nome) == 0)
-					printf("\nProduto nao cadastrado!\n");
-				else
-					printf("\n%s, R$ %.2f, L: %.2fm x P: %.2fm x A: %.2fm", p[op_2-1].nome, p[op_2-1].preco, p[op_2-1].dimensoes.largura, p[op_2-1].dimensoes.profundidade, p[op_2-1].dimensoes.altura);
-				break;
-		}
-	}while (op_1 == 1 || op_1 == 2);
-	return 0;
+Dimensao;
+
+typedef struct Produto{
+    char nomePro[64];
+    float preco;
+    Dimensao dimensao;
+}Produto;
+
+void cadastra(Produto * p){
+
+    printf("\nDigite o nome do produto: ");
+    fgets(p->nomePro, 64, stdin);
+    p->nomePro[strlen(p->nomePro)-1] = '\0';
+
+    printf("\nDigite o preco: ");
+    scanf("%f", &(p->preco));
+    getchar();
+
+    printf("\nDigite a largura: ");
+    scanf("%f", &(p->dimensao.largura));
+    getchar();
+
+    printf("\nDigite a profundidade: ");
+    scanf("%f", &(p->dimensao.profundidade));
+    getchar();
+
+    printf("\nDigite a altura: ");
+    scanf("%f", &(p->dimensao.altura));
+}
+
+void consulta(Produto * p){
+    char c;
+
+    if(strlen(p->nomePro) == 0){
+        printf("\nProduto nao cadastrado!\n");
+        while((c=getchar()) != '\n');
+        return;
+    }
+
+    printf("\n%s, R$ %.2f, Height: %.2fm x Length: %.2fm x Width: %.2fm\n",
+    p->nomePro, p->preco,(p->dimensao.largura),(p->dimensao.profundidade), (p->dimensao.altura));
+    while((c=getchar())!='\n');
+}
+
+int main(){
+    int flag=0, flag2;
+    Produto * produtos = calloc(2, sizeof(Produto));
+    char str[2][16] = {"Cadastro", "Consulta"};
+
+    do{
+       printf("\n1 - Cadastrar\n2 - Consulta\n3 - Sair\n-- ");
+        scanf("%d", &flag);
+        getchar();
+
+        flag2 = 0;
+
+        	if(flag==1){
+        		printf("\nRealizar %s\n", str[flag-1]);
+         		printf("\n1 - Produto 1\n2 - Produto 2\n3 - Voltar\n-- ");
+   	     		scanf("%d", &flag2);
+   	     		getchar();
+
+   	     		switch(flag2){
+
+                        case 1:
+                            printf("\nProduto %d\n", flag2);
+                            cadastra(&produtos[flag2-1]);
+                          	break;
+
+                        case 2:
+                            printf("\nProduto %d\n", flag2);
+                            cadastra(&produtos[flag2-1]);
+                          	break;
+                    		}
+
+                    	}
+
+			if(flag==2){
+        		printf("\nRealizar %s\n", str[flag-1]);
+         		printf("\n1 - Produto 1\n2 - Produto 2\n3 - Voltar\n-- ");
+   	     		scanf("%d", &flag2);
+   	     		getchar();
+
+   	     		switch(flag2){
+
+                        case 1:
+                            printf("\nProduto %d\n", flag2);
+                            consulta(&produtos[flag2-1]);
+                          	break;
+
+                        case 2:
+                            printf("\nProduto %d\n", flag2);
+                            consulta(&produtos[flag2-1]);
+                          	break;
+                    		}
+
+						}
+	}while(flag!=3);
+
+	free(produtos);
+
+    return 0;
 }
